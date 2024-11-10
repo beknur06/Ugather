897515ef5965ea4abf0ee90e17c1f02f8fb6272d
Beknur <beknur.les06@gmail.com>
2024-11-10 12:44:08 +0500

    Update event types

--- src/main/java/ugather/model/EventType.java
+++ src/main/java/ugather/model/EventType.java
@@ -1,13 +1,12 @@
 package ugather.model;
 
 public enum EventType {
-    SPORTS,
-    MUSIC,
-    ARTS,
-    THEATER,
-    FOOD,
-    CHARITY,
-    EDUCATION,
-    BUSINESS,
+    IT,
+    ENGINEERING,
+    ENTERTAINMENT,
+    WEBINAR,
+    WORKSHOP,
+    RESEARCH,
+    FASHION,
     OTHER
 }
